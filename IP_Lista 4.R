607f2d443266b17eaaddbf23d0740c5f9dd93844
Pedro Pinto <pedro.pinto885@gmail.com>
2016-11-23 12:13:59 -0200

    Update IP_Lista 4.R

--- IP_Lista 4.R
+++ IP_Lista 4.R
@@ -1,4 +1,3 @@
-#===============================================================================
 ################## Lista 4 ####################
 
 #Documentação:
@@ -46,75 +45,92 @@
 #Seção 7 - Fim (0) = 33*
 
 
-main<-function(amostras=1){
+main<-function(amostras){
   
   #Biblioteca
   library(igraph)
   library(triangle)
   
-  grafoCompleto<-make_graph(c(1,2,1,4,2,3,4,5,4,6,5,6,6,7,7,8,2,8,3,8,
-                      8,9,9,10,9,11,10,12,11,12,12,13,12,14,13,30,14,30,
-                      8,15,15,16,16,17,16,18,17,19,18,19,19,20,19,21,20,30,21,30,
-                      15,22,22,23,23,24,23,25,24,26,25,26,26,27,26,28,27,30,28,30,
-                      22,29,29,30,
-                      30,31,30,33,31,32,32,33
-                      ))
   
-  grafoBase<-make_graph(c(1,2,1,4,6,7,7,8,8,9,12,13,12,14,13,30,14,30,
-                          8,15,15,16,19,20,19,21,20,30,21,30,
-                          15,22,22,23,26,27,26,28,27,30,28,30,
-                          22,29,29,30
-                          ))
   
+  grafoCompleto<-(c(1,2,1,4,2,3,4,5,4,6,5,6,6,7,7,8,2,8,3,8,
+                    8,9,9,10,9,11,10,12,11,12,12,13,12,14,13,30,14,30,
+                    8,15,15,16,16,17,16,18,17,19,18,19,19,20,19,21,20,30,21,30,
+                    15,22,22,23,23,24,23,25,24,26,25,26,26,27,26,28,27,30,28,30,
+                    22,29,29,30,
+                    30,31,30,33,31,32,32,33
+                    ))
+  
+  grafoBase<-(c(1,2,1,4,6,7,7,8,8,9,12,13,12,14,13,30,14,30,
+                8,15,15,16,19,20,19,21,20,30,21,30,
+                15,22,22,23,26,27,26,28,27,30,28,30,
+                22,29,29,30
+                ))
+  duracao<-vector(length = amostras)
+  custo<-vector(length = amostras)
   caminho<-grafoBase
   evento<-eventos(amostras)
+  tempo<-prazo(amostras)
+  preco<-custos(tempo, amostras)
   for(i in 1:amostras){
+    custo[i]<-sum(preco[i,])
     if (evento[i,1]==1){
-      caminho<-caminho+edges(2,3,3,8)
+      caminho<-c(caminho,c(2,3,3,8))
     }
     else{
-      caminho<-caminho+edges(2,8)
+      caminho<-c(caminho,c(2,8))
+      custo[i]<-custo[i]-preco[i,3]
     }
     if (evento[i,2]==1){
-      caminho<-caminho+edges(4,5,5,6)
+      caminho<-c(caminho,c(4,5,5,6))
     }
     else{
-      caminho<-caminho+edges(4,6)
+      caminho<-c(caminho,c(4,6))
+      custo[i]<-custo[i]-preco[i,5]
     }
     
     if (evento[i,3]==1){
-      caminho<-caminho+edges(9,11,11,12,16,18,18,19,23,25,25,26)
+      caminho<-c(caminho,c(9,11,11,12,16,18,18,19,23,25,25,26))
+      custo[i]<-custo[i]-preco[i,10]-preco[i,17]-preco[i,24]
     }
     else{
-      caminho<-caminho+edges(9,10,10,12,16,17,17,19,23,24,24,26)
+      caminho<-c(caminho,c(9,10,10,12,16,17,17,19,23,24,24,26))
+      custo[i]<-custo[i]-preco[i,11]-preco[i,18]-preco[i,25]
     }
     
     if (evento[i,4]==1){
-      caminho<-caminho+edges(30,31)
+      caminho<-c(caminho,c(30,31))
       if (evento[i,5]==1){
-        caminho<-caminho+edges(31,32)
+        caminho<-c(caminho,c(31,32,32,33))
       }
       else{
-        caminho<-caminho+edges(31,33)
+        caminho<-c(caminho,c(31,33))
+        custo[i]<-custo[i]-preco[i,32]
       }
     }
     else{
-      caminho<-caminho+edges(30,33)
+      caminho<-c(caminho,c(30,33))
+      custo[i]<-custo[i]-preco[i,31]-preco[i,32]
     }
+    
+    g<-make_graph(caminho)
+    p<-all_simple_paths(g, from=1, to=33)
+    np<-length(p)
+    maior<-0
+    for (j in 1:np){
+      t<-sum(tempo[i,][p[[j]]])
+      if(t>maior){
+        maior<-t
+      }
+
+    }
+    
+    
+    duracao[i]<-maior
   }
-  plot(caminho)
+  list(mean(custo), var(custo), mean(duracao), var(duracao))
   
-  # lista<-all_simple_paths(grafoNOif,from=1,to=33) 
-  # plot(grafoCompleto)
-  
-  #if()....
-  
-  
-  #a partir dos eventos monta o grafo e retorna o grafo novo  
-  #e os nós participantes do cenários de cada caminho
-  #         somacaminho<-sum(grafo[i,][nósparticipantes])
-  
-  
+
 }
 
 eventos<-function(amostras){
@@ -130,7 +146,7 @@ eventos<-function(amostras){
   neventos
 }
                   
-prazo<-function(){
+prazo<-function(amostras){
   
   nprazo<-matrix(nrow = amostras, ncol = 33)
   
@@ -141,7 +157,7 @@ prazo<-function(){
   nprazo[,5]<-rtriangle(amostras,8,14,10)
   nprazo[,6]<-rtriangle(amostras,6,8,7)
   nprazo[,7]<-rtriangle(amostras,3,6,4)
-  nprazo[,8]<-rtriangle(amostras,4,4.5,6)
+  nprazo[,8]<-rtriangle(amostras,4,6,4.5)
   nprazo[,9]<-3
   nprazo[,10]<-rtriangle(amostras,7,9,8)
   nprazo[,11]<-rtriangle(amostras,6,11,8)
@@ -167,9 +183,11 @@ prazo<-function(){
   nprazo[,31]<-rtriangle(amostras,0.2,5,2)
   nprazo[,32]<-rtriangle(amostras,0.5,1.5,1)
   nprazo[,33]<-0
+  
+  nprazo
 }
 
-custos<-function(pr){
+custos<-function(pr, amostras){
   
   ncusto<-matrix(nrow = amostras, ncol = 33)
   
@@ -206,4 +224,6 @@ custos<-function(pr){
   ncusto[,31]<-0
   ncusto[,32]<-0
   ncusto[,33]<-0
+  
+  ncusto
 }
